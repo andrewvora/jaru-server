e66d192cc146ece541c853dc470882cd1b7755c3
Andrew Vorakrajangthiti <andrewjvora@gmail.com>
2019-01-16 01:19:53 -0800

    Allow CORS for PUT/POST/DELETE/GET
    Give LearningSetDto fields a serialization key.

--- src/main/kotlin/com/andrewvora/jaru/api/models/LearningSetDto.kt
+++ src/main/kotlin/com/andrewvora/jaru/api/models/LearningSetDto.kt
@@ -1,5 +1,7 @@
 package com.andrewvora.jaru.api.models
 
+import org.codehaus.jackson.annotate.JsonProperty
+
 data class LearningSetDto(
-		val questionSets: MutableList<QuestionSetDto> = mutableListOf()
+		@JsonProperty("sets") val questionSets: MutableList<QuestionSetDto> = mutableListOf()
 )
--- src/main/kotlin/com/andrewvora/jaru/security/SecurityConfigurer.kt
+++ src/main/kotlin/com/andrewvora/jaru/security/SecurityConfigurer.kt
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -35,7 +36,13 @@ constructor(
 
 	override fun configure(http: HttpSecurity?) {
 		http?.cors()?.configurationSource {
-			CorsConfiguration().applyPermitDefaultValues()
+			CorsConfiguration().applyPermitDefaultValues().apply {
+				allowedMethods = listOf(
+						HttpMethod.DELETE.name,
+						HttpMethod.GET.name,
+						HttpMethod.POST.name,
+						HttpMethod.PUT.name)
+			}
 		}
 		http?.csrf()?.disable()
 				?.sessionManagement()
